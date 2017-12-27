8fccc5f2a2586af8971c58781370980371f34c96
Brendan J. Herger <b@revup.com>
2017-12-27 11:53:54 -0800

    Working lstm model

--- bin/main.py
+++ bin/main.py
@@ -79,7 +79,7 @@ def model(observations, X, y):
                  ModelCheckpoint(mc_log_path)]
 
     # Create model
-    bool_model = models.cnn_embedding(X, y)
+    bool_model = models.lstm_embedding(X, y)
 
     # Fit model
     bool_model.fit(X, y, callbacks=callbacks, validation_split=.2)
--- bin/models.py
+++ bin/models.py
@@ -9,7 +9,7 @@ import keras
 import numpy
 from keras import Model
 from keras.layers import Embedding, Dense, LSTM, Conv1D, MaxPooling1D, Flatten
-from keras.optimizers import Adam
+from keras.optimizers import Adam, RMSprop
 
 import lib
 
@@ -68,7 +68,53 @@ def cnn_embedding(X, y):
     return bool_model
 
 def lstm_embedding(X, y):
-    pass
+    # Create Input layer
+    # Input: Input length
+    if len(X.shape) >= 2:
+        model_input_length = int(X.shape[1])
+    else:
+        model_input_length = 1
+
+    # Create Embedding layer
+    # Embedding: Embedding input dimensionality is the same as the number of classes in the input data set
+    embedding_input_dim = max(len(lib.legal_characters()), numpy.max(X)) + 1
+
+    # Embedding: Embedding output dimensionality is determined by heuristic
+    embedding_output_dim = int(min((embedding_input_dim + 1) / 2, 50))
+
+    # Input: Use a smaller datatype, if possible. This explicit typing is necessary due to the OHE layer.
+    if embedding_input_dim < 250:
+        dtype = 'uint8'
+    else:
+        dtype = 'int32'
+
+    # Create output layer
+    # Output: Output shape
+    if len(y.shape) >= 2:
+        softmax_output_dim = y.shape[1]
+    else:
+        softmax_output_dim = 1
+
+    sequence_input = keras.Input(shape=(model_input_length,), dtype=dtype, name='char_input')
+
+    embedding_layer = Embedding(input_dim=embedding_input_dim,
+                                output_dim=embedding_output_dim,
+                                input_length=model_input_length,
+                                trainable=True,
+                                name='char_embedding')
+
+    output_layer = Dense(units=softmax_output_dim, activation='softmax')
+
+    x = sequence_input
+    x = embedding_layer(x)
+    x = LSTM(128)(x)
+    x = output_layer(x)
+
+    optimizer = RMSprop(lr=.001)
+    bool_model = Model(sequence_input, x)
+    bool_model.compile(optimizer=optimizer, loss='binary_crossentropy')
+
+    return bool_model
 
 def cnn_lstm_embedding(X, y):
     pass
